58a751a61c0cc4a7e460416af257123bccc3689f
Alghany Jagad <alghanyjagad@gmail.com>
2021-05-21 14:28:25 +0700

    adding first template

---
+++ public/js/custom.js
@@ -0,0 +1,267 @@
+(function($) {
+'use strict';
+    // Roles data table
+    $(document).ready(function()
+    {
+        var searchable = [];
+        var selectable = []; 
+        
+
+        var dTable = $('#roles_table').DataTable({
+
+            order: [],
+            lengthMenu: [[10, 25, 50, 100, -1], [10, 25, 50, 100, "All"]],
+            processing: true,
+            responsive: false,
+            serverSide: true,
+            processing: true,
+            language: {
+              processing: '<i class="ace-icon fa fa-spinner fa-spin orange bigger-500" style="font-size:60px;margin-top:50px;"></i>'
+            },
+            scroller: {
+                loadingIndicator: false
+            },
+            pagingType: "full_numbers",
+            dom: "<'row'<'col-sm-2'l><'col-sm-7 text-center'B><'col-sm-3'f>>tipr",
+            ajax: {
+                url: 'role/get-list',
+                type: "get"
+            },
+            columns: [
+                {data:'name', name: 'name', orderable: false, searchable: false},
+                {data:'permissions', name: 'permissions'},
+                //only those have manage_role permission will get access
+                {data:'action', name: 'action'}
+
+            ],
+            buttons: [
+                {
+                    extend: 'copy',
+                    className: 'btn-sm btn-info',
+                    title: 'Roles',
+                    header: false,
+                    footer: true,
+                    exportOptions: {
+                        // columns: ':visible'
+                    }
+                },
+                {
+                    extend: 'csv',
+                    className: 'btn-sm btn-success',
+                    title: 'Roles',
+                    header: false,
+                    footer: true,
+                    exportOptions: {
+                        // columns: ':visible'
+                    }
+                },
+                {
+                    extend: 'excel',
+                    className: 'btn-sm btn-warning',
+                    title: 'Roles',
+                    header: false,
+                    footer: true,
+                    exportOptions: {
+                        // columns: ':visible',
+                    }
+                },
+                {
+                    extend: 'pdf',
+                    className: 'btn-sm btn-primary',
+                    title: 'Roles',
+                    pageSize: 'A2',
+                    header: false,
+                    footer: true,
+                    exportOptions: {
+                        // columns: ':visible'
+                    }
+                },
+                {
+                    extend: 'print',
+                    className: 'btn-sm btn-default',
+                    title: 'Roles',
+                    // orientation:'landscape',
+                    pageSize: 'A2',
+                    header: true,
+                    footer: false,
+                    orientation: 'landscape',
+                    exportOptions: {
+                        // columns: ':visible',
+                        stripHtml: false
+                    }
+                }
+            ],
+            initComplete: function () {
+                var api =  this.api();
+                api.columns(searchable).every(function () {
+                    var column = this;
+                    var input = document.createElement("input");
+                    input.setAttribute('placeholder', $(column.header()).text());
+                    input.setAttribute('style', 'width: 140px; height:25px; border:1px solid whitesmoke;');
+
+                    $(input).appendTo($(column.header()).empty())
+                    .on('keyup', function () {
+                        column.search($(this).val(), false, false, true).draw();
+                    });
+
+                    $('input', this.column(column).header()).on('click', function(e) {
+                        e.stopPropagation();
+                    });
+                });
+
+                api.columns(selectable).every( function (i, x) {
+                    var column = this;
+
+                    var select = $('<select style="width: 140px; height:25px; border:1px solid whitesmoke; font-size: 12px; font-weight:bold;"><option value="">'+$(column.header()).text()+'</option></select>')
+                        .appendTo($(column.header()).empty())
+                        .on('change', function(e){
+                            var val = $.fn.dataTable.util.escapeRegex(
+                                $(this).val()
+                            );
+                            column.search(val ? '^'+val+'$' : '', true, false ).draw();
+                            e.stopPropagation();
+                        });
+
+                    $.each(dropdownList[i], function(j, v) {
+                        select.append('<option value="'+v+'">'+v+'</option>')
+                    });
+                });
+            }
+        });
+    });
+
+    //users data table
+    $(document).ready(function()
+    {
+
+        var searchable = [];
+        var selectable = []; 
+        
+
+        var dTable = $('#user_table').DataTable({
+
+            order: [],
+            lengthMenu: [[10, 25, 50, 100, -1], [10, 25, 50, 100, "All"]],
+            processing: true,
+            responsive: false,
+            serverSide: true,
+            processing: true,
+            language: {
+              processing: '<i class="ace-icon fa fa-spinner fa-spin orange bigger-500" style="font-size:60px;margin-top:50px;"></i>'
+            },
+            scroller: {
+                loadingIndicator: false
+            },
+            pagingType: "full_numbers",
+            dom: "<'row'<'col-sm-2'l><'col-sm-7 text-center'B><'col-sm-3'f>>tipr",
+            ajax: {
+                url: 'user/get-list',
+                type: "get"
+            },
+            columns: [
+                /*{data:'serial_no', name: 'serial_no'},*/
+                {data:'name', name: 'name', orderable: false, searchable: false},
+                {data:'email', name: 'email'},
+                {data:'roles', name: 'roles'},
+                {data:'permissions', name: 'permissions'},
+                //only those have manage_user permission will get access
+                {data:'action', name: 'action'}
+
+            ],
+            buttons: [
+                {
+                    extend: 'copy',
+                    className: 'btn-sm btn-info',
+                    title: 'Users',
+                    header: false,
+                    footer: true,
+                    exportOptions: {
+                        // columns: ':visible'
+                    }
+                },
+                {
+                    extend: 'csv',
+                    className: 'btn-sm btn-success',
+                    title: 'Users',
+                    header: false,
+                    footer: true,
+                    exportOptions: {
+                        // columns: ':visible'
+                    }
+                },
+                {
+                    extend: 'excel',
+                    className: 'btn-sm btn-warning',
+                    title: 'Users',
+                    header: false,
+                    footer: true,
+                    exportOptions: {
+                        // columns: ':visible',
+                    }
+                },
+                {
+                    extend: 'pdf',
+                    className: 'btn-sm btn-primary',
+                    title: 'Users',
+                    pageSize: 'A2',
+                    header: false,
+                    footer: true,
+                    exportOptions: {
+                        // columns: ':visible'
+                    }
+                },
+                {
+                    extend: 'print',
+                    className: 'btn-sm btn-default',
+                    title: 'Users',
+                    // orientation:'landscape',
+                    pageSize: 'A2',
+                    header: true,
+                    footer: false,
+                    orientation: 'landscape',
+                    exportOptions: {
+                        // columns: ':visible',
+                        stripHtml: false
+                    }
+                }
+            ],
+            initComplete: function () {
+                var api =  this.api();
+                api.columns(searchable).every(function () {
+                    var column = this;
+                    var input = document.createElement("input");
+                    input.setAttribute('placeholder', $(column.header()).text());
+                    input.setAttribute('style', 'width: 140px; height:25px; border:1px solid whitesmoke;');
+
+                    $(input).appendTo($(column.header()).empty())
+                    .on('keyup', function () {
+                        column.search($(this).val(), false, false, true).draw();
+                    });
+
+                    $('input', this.column(column).header()).on('click', function(e) {
+                        e.stopPropagation();
+                    });
+                });
+
+                api.columns(selectable).every( function (i, x) {
+                    var column = this;
+
+                    var select = $('<select style="width: 140px; height:25px; border:1px solid whitesmoke; font-size: 12px; font-weight:bold;"><option value="">'+$(column.header()).text()+'</option></select>')
+                        .appendTo($(column.header()).empty())
+                        .on('change', function(e){
+                            var val = $.fn.dataTable.util.escapeRegex(
+                                $(this).val()
+                            );
+                            column.search(val ? '^'+val+'$' : '', true, false ).draw();
+                            e.stopPropagation();
+                        });
+
+                    $.each(dropdownList[i], function(j, v) {
+                        select.append('<option value="'+v+'">'+v+'</option>')
+                    });
+                });
+            }
+        });
+    });
+    $('select').select2();
+})(jQuery);
