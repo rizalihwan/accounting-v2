7c3f768ec5d93f67bd3035d2ee6af7de4a350aba
rizalihwan <55536560+rizalihwan@users.noreply.github.com>
2021-05-30 21:21:48 +0700

    fix route

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 //auth management
 Route::get('/', 'AuthController@loginView');
 Route::post('/login', 'AuthController@login')->name('login');
+Route::post('/logout', 'AuthController@logout')->name('logout');
 Route::get('register', function () {
     return view('_auth/register');
 });
