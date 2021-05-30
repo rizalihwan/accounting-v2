f8b3c4b5b2b515fd651cc0c429c16595f9b0fdc8
agungprasetio18 <agungprasetio878@gmail.com>
2021-05-30 14:58:39 +0700

    login

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('_auth/login');
-});
+//auth login
+Route::get('/login', 'AuthController@loginView');
+Route::post('/login', 'AuthController@login')->name('login');
 
 Route::get('register', function () {
     return view('_auth/register');
