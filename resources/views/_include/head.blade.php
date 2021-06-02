d2cb8de21ee20a1a8563ec9571d2ceba825d16b5
Muhammad Ramdhani <mramdhani010@gmail.com>
2021-06-02 22:16:21 +0700

    css loader

--- resources/views/_include/head.blade.php
+++ resources/views/_include/head.blade.php
@@ -25,4 +25,54 @@
 @livewireStyles()
 
 <!-- Stack array for including inline css or head elements -->
+<style>
+    /* Loader Animation */
+    .inputcontainer {
+        position: relative;
+    }
+    .icon-container {
+        position: absolute;
+        right: 0px;
+        top: calc(50% - 10px);
+    }
+    .loader {
+        position: relative;
+        height: 20px;
+        width: 20px;
+        display: inline-block;
+        animation: around 5.4s infinite;
+    }
+
+    @keyframes around {
+        0% {
+            transform: rotate(0deg)
+        }
+        100% {
+            transform: rotate(360deg)
+        }
+    }
+
+    .loader::after, .loader::before {
+        content: "";
+        background: white;
+        position: absolute;
+        display: inline-block;
+        width: 100%;
+        height: 100%;
+        border-width: 2px;
+        border-color: rgb(100, 100, 100) rgb(100, 100, 100) transparent transparent;
+        border-style: solid;
+        border-radius: 20px;
+        box-sizing: border-box;
+        top: 0;
+        left: 0;
+        animation: around 0.7s ease-in-out infinite;
+    }
+
+    .loader::after {
+        animation: around 0.7s ease-in-out 0.1s infinite;
+        background: transparent;
+    }
+</style>
+
 @stack('head')
