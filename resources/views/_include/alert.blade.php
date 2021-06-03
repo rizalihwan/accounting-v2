6f77a0d1f5e93f4020a421dea43da200824e60cb
Muhammad Ramdhani <mramdhani010@gmail.com>
2021-06-04 02:50:15 +0700

    bikin event sweetalert untuk dipakai dilivewire

--- resources/views/_include/alert.blade.php
+++ resources/views/_include/alert.blade.php
@@ -21,3 +21,29 @@
 
     </script>
 @endif
+
+<script>
+    window.addEventListener('swal:modal', event => {
+        Swal.fire({
+            icon: event.detail.type,
+            title: event.detail.title,
+            text: event.detail.text
+        })
+    })
+
+    window.addEventListener('swal:confirm', event => {
+        Swal.fire({
+            icon: event.detail.type,
+            title: event.detail.title,
+            text: event.detail.text,
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Ya, hapus ini!'
+        }).then((willDelete) => {
+            if (willDelete.isConfirmed) {
+                window.livewire.emit('delete', event.detail.id)
+            }
+        })
+    })
+</script>
