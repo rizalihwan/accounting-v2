7cc2f0a4acb2959dd307f8f722d8f293443d6fc7
Alghany Jagad <alghanyjagad@gmail.com>
2021-06-06 21:49:37 +0700

    migration kontak

--- database/migrations/2021_06_02_100228_create_kontaks_table.php
+++ database/migrations/2021_06_02_100228_create_kontaks_table.php
@@ -15,17 +15,22 @@ class CreateKontaksTable extends Migration
     {
         Schema::create('kontaks', function (Blueprint $table) {
             $table->id();
-            $table->string('kode');
             $table->string('nama');
-            $table->foreignId('kategori_id')->constrained();
+            $table->string('email');
+            $table->string('telepon')->nullable();
+            $table->boolean('pelanggan');
+            $table->boolean('pemasok');
+            $table->boolean('karyawan');
             $table->longText('alamat')->nullable();
             $table->string('kota')->nullable();
             $table->string('kode_pos')->nullable();
-            $table->string('telepon')->nullable();
-            $table->string('fax')->nullable();
-            $table->string('bank')->nullable();
-            $table->string('ac')->nullable();
-            $table->string('catatan')->nullable();
+            $table->string('negara')->nullable();
+            $table->boolean('setel_alamat_pengiriman');
+            $table->string('kode_kontak');
+            $table->string('mata_uang')->nullable();
+            $table->string('nik')->nullable();
+            $table->string('kontak_person')->nullable();
+            $table->string('website')->nullable();
             $table->timestamps();
         });
     }
