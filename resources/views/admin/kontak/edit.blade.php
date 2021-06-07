83e1a2b2a185c1afbe0bb3bb38547b0559515d8f
Ichsan_arrizqi <ichsanarrizqi@smkwikrama.sch.id>
2021-06-07 21:59:06 +0700

    crud kontak

--- resources/views/admin/kontak/edit.blade.php
+++ resources/views/admin/kontak/edit.blade.php
@@ -34,14 +34,15 @@
                         <h3>Tambah Kontak Person</h3>
                     </div>
                     <div class="card-body">
-                        <form class="forms-sample" method="POST" action="{{ route('admin.kontak.store') }}">
+                        <form class="forms-sample" method="POST" action="{{ route('admin.kontak.update', $kontak->id) }}">
                             @csrf
+                            @method('PUT')
                             <p><strong>Umum</strong></p>
                             <div class="row">
                                 <div class="col-sm-4">
                                     <div class="form-group">
                                         <label for="nama">{{ __('Nama') }}<span class="text-red">*</span></label>
-                                        <input id="nama" type="text"
+                                        <input id="nama" type="text" value="{{$kontak->nama}}"
                                             class="form-control @error('nama') is-invalid @enderror" name="nama" required>
                                         <div class="help-block with-errors"></div>
                                         @error('nama')
@@ -55,7 +56,7 @@
                                 <div class="col-sm-4">
                                     <div class="form-group">
                                         <label for="email">{{ __('Email') }}<span class="text-red">*</span></label>
-                                        <input id="email" type="email"
+                                        <input id="email" type="email" value="{{$kontak->email}}"
                                             class="form-control @error('email') is-invalid @enderror" name="email" required>
                                         <div class="help-block with-errors"></div>
                                         @error('email')
@@ -69,7 +70,7 @@
                                 <div class="col-sm-12">
                                     <div class="form-group">
                                         <label for="telepon">Telepon<span class="text-red">*</span></label>
-                                        <input id="telepon" type="text"
+                                        <input id="telepon" type="text" value="{{$kontak->telepon}}"
                                             class="form-control @error('telepon') is-invalid @enderror" name="telepon"
                                             minlength="11" maxlength="13" onkeypress="return hanyaAngka(event)">
                                         <div class="help-block with-errors"></div>
@@ -84,15 +85,15 @@
                                     <div class="form-group">
                                         <div class="border-checkbox-section">
                                             <div class="border-checkbox-group border-checkbox-group-primary">
-                                                <input class="border-checkbox" type="checkbox" id="pelanggan" name="pelanggan" checked="checked">
+                                                <input class="border-checkbox" type="checkbox" id="pelanggan" name="pelanggan" {{ ($kontak->pelanggan == 1 ? ' checked' : '') }}>
                                                 <label class="border-checkbox-label" for="pelanggan">{{ __('Pelanggan')}}</label>
                                             </div>
                                             <div class="border-checkbox-group border-checkbox-group-primary">
-                                                <input class="border-checkbox" type="checkbox" id="pemasok" name="pemasok" checked="checked">
+                                                <input class="border-checkbox" type="checkbox" id="pemasok" name="pemasok" {{ ($kontak->pemasok == 1 ? ' checked' : '') }}>
                                                 <label class="border-checkbox-label" for="pemasok">{{ __('Pemasok')}}</label>
                                             </div>
                                             <div class="border-checkbox-group border-checkbox-group-primary">
-                                                <input class="border-checkbox" type="checkbox" id="karyawan" name="karyawan" checked="checked">
+                                                <input class="border-checkbox" type="checkbox" id="karyawan" name="karyawan" {{ ($kontak->karyawan == 1 ? ' checked' : '') }}>
                                                 <label class="border-checkbox-label" for="karyawan">{{ __('Karyawan')}}</label>
                                             </div>
                                         </div>
@@ -106,9 +107,7 @@
                                     <div class="form-group">
                                         <label for="alamat">alamat</label>
                                         <textarea name="alamat" id="alamat"
-                                            class="form-control @error('alamat') is-invalid @enderror">
-
-                                                    </textarea>
+                                            class="form-control @error('alamat') is-invalid @enderror">{{$kontak->alamat}}</textarea>
                                         <div class="help-block with-errors"></div>
                                         @error('alamat')
                                             <span class="invalid-feedback" role="alert">
@@ -121,7 +120,7 @@
                                 <div class="col-sm-6">
                                     <div class="form-group">
                                         <label for="kota">kota</label>
-                                        <input id="kota" type="text"
+                                        <input id="kota" type="text" value="{{$kontak->kota}}"
                                             class="form-control @error('kota') is-invalid @enderror" name="kota">
                                         <div class="help-block with-errors"></div>
                                         @error('kota')
@@ -135,7 +134,7 @@
                                 <div class="col-sm-6">
                                     <div class="form-group">
                                         <label for="kode_pos">kode pos</label>
-                                        <input id="kode_pos" type="text"
+                                        <input id="kode_pos" type="text" value="{{$kontak->kode_pos}}"
                                             class="form-control @error('kode_pos') is-invalid @enderror" name="kode_pos">
                                         <div class="help-block with-errors"></div>
                                         @error('kode_pos')
@@ -152,7 +151,7 @@
                                 <div class="col-sm-6">
                                     <div class="form-group">
                                         <label for="kode_kontak">Kode Kontak</label>
-                                        <input id="kode_kontak" type="text"
+                                        <input id="kode_kontak" type="text" value="{{$kontak->kode_kontak}}"
                                             class="form-control @error('kode_kontak') is-invalid @enderror" name="kode_kontak" required>
                                         <div class="help-block with-errors"></div>
                                         @error('kode_kontak')
@@ -165,7 +164,7 @@
                                 <div class="col-sm-6">
                                     <div class="form-group">
                                         <label for="mata_uang">{{ __('Mata Uang') }}</label>
-                                        <select name="mata_uang" id="mata_uang"
+                                        <select name="mata_uang" id="mata_uang" value="{{$kontak->mata_uang}}"
                                             class="form-control @error('mata_uang') is-invalid @enderror">
                                             <option value="IDR">IDR</option>
                                         </select>
@@ -181,7 +180,7 @@
                                 <div class="col-sm-4">
                                     <div class="form-group">
                                         <label for="nik">NIK</label>
-                                        <input id="nik" type="text"
+                                        <input id="nik" type="text" value="{{$kontak->nik}}"
                                             class="form-control @error('nik') is-invalid @enderror" name="nik"
                                             onkeypress="return hanyaAngka(event)" minlength="16" maxlength="16">
                                         <div class="help-block with-errors"></div>
@@ -196,7 +195,7 @@
                                 <div class="col-sm-4">
                                     <div class="form-group">
                                         <label for="kontak_person">Kontak Person</label>
-                                        <input id="kontak_person" type="text"
+                                        <input id="kontak_person" type="text" value="{{$kontak->kontak_person}}"
                                             class="form-control @error('kontak_person') is-invalid @enderror" name="kontak_person">
                                         <div class="help-block with-errors"></div>
                                         @error('kontak_person')
@@ -210,7 +209,7 @@
                                 <div class="col-sm-4">
                                     <div class="form-group">
                                         <label for="website">Website</label>
-                                        <input id="website" type="text"
+                                        <input id="website" type="text" value="{{$kontak->website}}"
                                             class="form-control @error('website') is-invalid @enderror" name="website">
                                         <div class="help-block with-errors"></div>
                                         @error('website')
@@ -223,7 +222,7 @@
 
                                 <div class="col-sm-12 col-xl-4 mb-30">
                                     <p><strong>Aktif</strong></p>
-                                    <input type="checkbox" class="js-single" name="aktif" checked="checked" />
+                                    <input type="checkbox" class="js-single" name="aktif" {{ ($kontak->aktif == 1 ? ' checked' : '') }} />
                                 </div>
                             </div>
 
@@ -233,7 +232,7 @@
                                         <a href="{{ route('admin.kontak.index') }}"
                                             class="btn btn-danger">KEMBALI</a>
                                         <button type="submit" class="btn btn-primary">
-                                            TAMBAH</button>
+                                            UPDATE</button>
                                     </div>
                                 </div>
                             </div>
