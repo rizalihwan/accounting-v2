ac24fefbd0cd703306bafd234dd5e44c524b2788
agungprasetio18 <agungprasetio878@gmail.com>
2021-06-13 23:09:19 +0700

    show

---
+++ resources/views/admin/product/show.blade.php
@@ -0,0 +1,34 @@
+@extends('_layouts.main')
+@section('title', 'Data Product')
+@push('breadcrumb')
+<li class="breadcrumb-item">
+    <a href="{{ route('admin.data-store') }}">Data Store</a>
+</li>
+<li class="breadcrumb-item">
+    <a href="{{ route('admin.product.index') }}">Product</a>
+</li>
+<li class="breadcrumb-item" aria-current="page">Show Product</li>
+@endpush
+@section('content')
+
+<div class="row">
+    <div class="card">
+        <!-- Product Details starts -->
+        <div class="card-body">
+            <div class="row my-2">
+                <div class="col-12 col-md-12">
+                    <h4>{{ $product->name }}</h4>
+                    <span class="card-text item-company">Supplier <a href="javascript:void(0)"
+                            class="company-name">{{ $product->supplier->nama }}</a></span>
+                    <div class="ecommerce-details-price d-flex flex-wrap mt-1">
+                        <h4 class="item-price mr-1">Harga Beli : {{ 'Rp. ' . number_format($product->price_buy, 0, ',', '.') }}</h4>
+                        <h4 class="item-price mr-1">Harga Jual : {{ 'Rp. ' . number_format($product->price_sell, 0, ',', '.') }}</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+@endsection
