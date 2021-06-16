5aa07a379926e4bb4706d8df114edb2099fd157c
Ramdhani <53980259+dhanifu@users.noreply.github.com>
2021-06-17 03:08:28 +0700

    edit footer

--- resources/views/_components/footer.blade.php
+++ resources/views/_components/footer.blade.php
@@ -1,3 +1,11 @@
-    {{-- <footer class="footer footer-light">
-      <p class="clearfix mb-0"><span class="float-md-left d-block d-md-inline-block mt-25">COPYRIGHT  &copy; 2020<a class="ml-25" href="https://1.envato.market/pixinvent_portfolio" target="_blank">Pixinvent</a><span class="d-none d-sm-inline-block">, All rights Reserved</span></span><span class="float-md-right d-none d-md-block">Hand-crafted & Made with<i data-feather="heart"></i></span></p>
-</footer> --}}
+<footer class="footer footer-light">
+    <p class="clearfix mb-0">
+        <span class="float-md-left d-block d-md-inline-block mt-25">
+            COPYRIGHT  &copy; <script>document.write(new Date().getFullYear());</script>
+            <a class="ml-25" href="javascript:void(0)">Ditkuad</a>
+        </span>
+        <span class="float-md-right d-none d-md-block">
+            Powered by aksimaya
+        </span>
+    </p>
+</footer>
