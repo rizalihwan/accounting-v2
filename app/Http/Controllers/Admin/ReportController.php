1fe579ccf256b016a540b16e570e3b473ed21c4b
rizalihwan <55536560+rizalihwan@users.noreply.github.com>
2021-06-19 23:07:44 +0700

    laporan jurnal umum done

--- app/Http/Controllers/Admin/ReportController.php
+++ app/Http/Controllers/Admin/ReportController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Jurnalumum;
 use Illuminate\Http\Request;
 
 class ReportController extends Controller
@@ -15,5 +16,19 @@ class ReportController extends Controller
     public function jurnalumum()
     {
         return view('report.keuangan.jurnalumum');
+    }
+
+    public function jurnalumumcari(Request $request)
+    {
+        $request->validate([
+            'startDate'=> 'required',
+            'endDate'=> 'required',
+        ]);
+        $from = $request->startDate;
+        $to = $request->endDate;
+        $startDate = $from;
+        $endDate = $to;
+        $data = Jurnalumum::whereBetween('created_at', [$startDate,$endDate])->with('jurnalumumdetails')->latest()->paginate(10);
+        return view('report.keuangan.jurnalumum', compact('data', 'startDate', 'endDate'));
     }
 }
--- resources/views/report/menu.blade.php
+++ resources/views/report/menu.blade.php
@@ -7,7 +7,7 @@
 @section('content') 
 <section id="card-content-types">
     <div class="row">
-        <a href="{{ route('admin.report.keuangan.jurnalumum') }}">Jurnal Umum</a>
+        <a href="{{ route('admin.report.keuangan.jurnalumum') }}" class="btn btn-success btn-sm"><i class="fa fa-book"></i> Jurnal Umum</a>
     </div>
 </section>
 @endsection
