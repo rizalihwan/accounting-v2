211ecf6bb0feb27bbe11e05c8ae225ce06447721
imamsalji <imamsalji7@gmail.com>
2021-06-27 22:16:07 +0700

    update

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -411,153 +411,78 @@
                             </a>
                         </div>
                     </div>
-                </div>
-            </a>
-        </div>
+                </section>
+            @endif
 
-        <div class="col-md-3 col-xl-3">
-            <a href="{{ route('admin.pengiriman.index') }}">
-                <div class="card bg-primary border-0 text-white shadow">
-                    <div class="card-body">
-                        <div class="title">
-                            <div class="row">
-                                <div class="col">
-                                    <h1 class="text-light">Pengiriman Barang</h1>
-                                </div>
-                                <div class="col">
-                                    <i class="fa fa-truck fa-lg fa-4x"></i>
-                                </div>
+            @if (request()->routeIs('admin.sales'))
+                @section('title', 'Penjualan')
+                    @push('breadcrumb')
+                        <li class="breadcrumb-item active">Penjualan</li>
+                    @endpush
+                    <section>
+                        <div class="row">
+                            <div class="col-md-3 col-xl-3">
+                                <a href="{{ route('admin.penawaran.index') }}">
+                                    <div class="card bg-success border-0 text-white shadow">
+                                        <div class="card-body">
+                                            <div class="title">
+                                                <div class="row">
+                                                    <div class="col">
+                                                        <h1 class="text-light">Penawaran Harga</h1>
+                                                    </div>
+                                                    <div class="col">
+                                                        <i class="fa fa-chart-line fa-lg fa-5x"></i>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="info">
+                                                <p class="card-text">
+                                                <h1 class="card-text display-5 text-white font-weight-bold">Penawaran Harga</h1>
+                                                <i class="fa fa-chart-line"></i>
+                                                </p>
+                                                <hr class="mr-1">
+                                                <p class="card-text">
+                                                    Membuat penawaran harga untuk pelanggan
+                                                </p>
+                                                <p class="card-text">
+                                                    <small class="text-light"></small>
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </a>
                             </div>
-                        </div>
-                        <div class="info">
-                            <p class="card-text">
-                            <h1 class="card-text display-5 text-white font-weight-bold">Pengiriman Barang</h1>
-                            <i class="fa fa-truck"></i>
-                            </p>
-                            <hr class="mr-1">
-                            <p class="card-text">
-                                Membuat pesanan pengiriman barang untuk pelanggan
-                            </p>
-                            <p class="card-text">
-                                <small class="text-light"></small>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </a>
-        </div>
-    </div>
-    </div>
-</section>
-@endif
 
-@if(request()->routeIs('admin.purchase'))
-@section('title', 'Purchases')
-@push('breadcrumb')
-<li class="breadcrumb-item active">Purchase</li>
-@endpush
-{{-- --}}
-@endif
-
-@if(request()->routeIs('admin.cash-bank'))
-@section('title', 'Cash & Bank')
-@push('breadcrumb')
-<li class="breadcrumb-item active">Cash & Bank</li>
-@endpush
-<section id="card-content-types">
-    <div class="row">
-        <div class="col-md-4">
-            <a href="{{ route('admin.bkk.index') }}">
-                <div class="card bg-warning border-0 text-white">
-                    <div class="card-body">
-                        <div class="title">
-                            <div class="row">
-                                <div class="col-md-10">
-                                    <h1 class="text-light">Pengeluaran</h1>
-                                </div>
-                                <div class="col-md-2">
-                                    <i class="fa fa-hand-holding-usd fa-lg fa-5x"></i>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="info">
-                            <p class="card-text">
-                            <h1 class="card-text display-5 text-white font-weight-bold">Pengeluaran</h1>
-                            <i class="fa fa-hand-holding-usd"></i>
-                            </p>
-                            <hr class="mr-1">
-                            <p class="card-text">
-                                Mencatat pengeluaran kas/bank
-                            </p>
-                            <p class="card-text">
-                                <small class="text-light"></small>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </a>
-        </div>
-        <div class="col-md-4">
-            <a href="{{ route('admin.bkm.index') }}">
-                <div class="card bg-success border-0 text-white">
-                    <div class="card-body">
-                        <div class="title">
-                            <div class="row">
-                                <div class="col-md-10">
-                                    <h1 class="text-light">Pemasukan</h1>
-                                </div>
-                                <div class="col-md-2">
-                                    <i class="fa fa-hand-holding-usd fa-lg fa-5x"></i>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="info">
-                            <p class="card-text">
-                            <h1 class="card-text display-5 text-white font-weight-bold">Pemasukan</h1>
-                            <i class="fa fa-hand-holding-usd"></i>
-                            </p>
-                            <hr class="mr-1">
-                            <p class="card-text">
-                                Mencatat pemasukan kas/bank
-                            </p>
-                            <p class="card-text">
-                                <small class="text-light"></small>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </a>
-        </div>
-    </div>
-</section>
-{{-- --}}
-@endif
-@if(request()->routeIs('admin.inventory'))
-@section('title', 'Inventory')
-@push('breadcrumb')
-<li class="breadcrumb-item active">Inventory</li>
-@endpush
-{{-- --}}
-@endif
-@if(request()->routeIs('admin.report.menu'))
-@section('title', 'Report')
-@push('breadcrumb')
-<li class="breadcrumb-item active">Report</li>
-@endpush
-<section id="card-content-types">
-    <div class="row">
-        <div class="col-md-3 col-xl-3">
-            <a href="{{ route('admin.report.keuangan.menu') }}">
-                <div class="card border-0 text-white" style="background-color: #72147E;">
-                    <div class="card-body">
-                        <div class="title">
-                            <div class="row">
-                                <div class="col">
-                                    <h1 class="text-light">Laporan Keuangan</h1>
-                                </div>
-                                <div class="col">
-                                    <i class="fa fa-trello fa-lg fa-5x"></i>
-                                </div>
+                            <div class="col-md-3 col-xl-3">
+                                <a href="{{ route('admin.pesanan.index') }}">
+                                    <div class="card bg-warning border-0 text-white shadow">
+                                        <div class="card-body">
+                                            <div class="title">
+                                                <div class="row">
+                                                    <div class="col">
+                                                        <h1 class="text-light">Pesanan Penjualan</h1>
+                                                    </div>
+                                                    <div class="col">
+                                                        <i class="fa fa-hand-holding-usd fa-lg fa-5x"></i>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="info">
+                                                <p class="card-text">
+                                                <h1 class="card-text display-5 text-white font-weight-bold">Pesanan Penjualan</h1>
+                                                <i class="fa fa-chart-line"></i>
+                                                </p>
+                                                <hr class="mr-1">
+                                                <p class="card-text">
+                                                    Membuat pesanan penjualan untuk pelanggan
+                                                </p>
+                                                <p class="card-text">
+                                                    <small class="text-light"></small>
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </a>
                             </div>
                             </a>
                         </div>
