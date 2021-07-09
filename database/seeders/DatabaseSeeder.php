c7bf2d5787a8c8d6bc57d199bc60a139d3355d50
Muhammad Ramdhani <mramdhani010@gmail.com>
2021-07-09 22:44:45 +0700

    unit seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,8 +14,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            AdminSeeder::class
+            AdminSeeder::class,
+            UnitSeeder::class,
         ]);
+
         // \App\Models\User::factory(10)->create();
     }
 }
