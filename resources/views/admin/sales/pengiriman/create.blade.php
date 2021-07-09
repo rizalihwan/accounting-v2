63969eea6a59b4255523ef21d570bdfbbf84cda5
Muhammad Ramdhani <mramdhani010@gmail.com>
2021-07-09 22:36:39 +0700

    fix tampilan

--- resources/views/admin/sales/pengiriman/create.blade.php
+++ resources/views/admin/sales/pengiriman/create.blade.php
@@ -12,6 +12,20 @@
 @section('content')
 <div class="row">
     <div class="col-md-12">
+        @if ($errors->any())
+            <div class="alert alert-danger alert-dismissible" role="alert">
+                <div class="alert-body">
+                    @foreach ($errors->all() as $error)
+                    <ul style="margin: 0 12px 0 -11px">
+                        <li>{{ $error }}</li>
+                    </ul>
+                    @endforeach
+                </div>
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+        @endif
         <form class="forms-sample" class="repeater" action="{{ route('admin.sales.pengiriman.store') }}" method="POST">
             <div class="card ">
                 <div class="card-body">
@@ -189,6 +203,7 @@
     $(document).ready(function () {
         $("#pelanggan_id").select2({
             placeholder: "-- Pilih Pelanggan --",
+            allowClear: true,
             ajax: {
                 url: '{{ route('api.select2.get-pelanggan') }}',
                 type: 'post',
@@ -198,9 +213,6 @@
                         _token: CSRF_TOKEN,
                         search: params.term
                     }
-                },
-                error: (err) => {
-                    console.log(err)
                 },
                 processResults: data => {
                     return {
@@ -214,6 +226,7 @@
         // Start PESANAN Select2
         $("#pesanan_id").select2({
             placeholder: "-- Pilih Pesanan --",
+            allowClear: true,
             ajax: {
                 url: '{{ route('api.select2.get-sale-pesanan') }}',
                 type: 'post',
@@ -223,9 +236,6 @@
                         _token: CSRF_TOKEN,
                         search: params.term
                     }
-                },
-                error: (err) => {
-                    console.log(err)
                 },
                 processResults: data => {
                     return {
@@ -238,26 +248,30 @@
         
 
         $('#pesanan_id').on('select2:select', function (e) {
-            $("#dynamic_field").html('')
-
             const data = e.params.data;
             const detail = data.detail;
+            let subtotal = 0;
+
+            $("#pesanan_id").attr('disabled', true)
 
             for (let index = 0; index < detail.length; index++) {
-                field_dinamis()
-
                 let product_id = detail[index].product_id;
                 let jumlah = detail[index].jumlah;
+                let satuan = detail[index].satuan;
+                let harga = detail[index].harga;
+                let total = detail[index].total;
+
                 let url_product = '{{ route('api.select2.get-product.selected', ':id') }}';
                 url_product = url_product.replace(':id', product_id);
+
+                $("#dynamic_field").html('')
 
                 $.ajax({
                     url: url_product,
                     type: 'get',
-                    error: (err) => {
-                        console.log(err);
-                    }
                 }).then((data) => {
+                    field_dinamis()
+
                     let option = new Option(data.text, data.id, true, true)
 
                     $('select[name="pengirimans['+index+'][product_id]"]').append(option).trigger('change')
@@ -268,10 +282,20 @@
                         }
                     })
 
-                    $('[name="pengirimans['+index+'][satuan]"]').val(unit)
-                    $('[name="pengirimans['+index+'][harga]"]').val(formatter(price))
                     $('[name="pengirimans['+index+'][harga]"]').attr('readonly', false)
                     $('[name="pengirimans['+index+'][jumlah]"]').attr('readonly', false)
+
+                    $('[name="pengirimans['+index+'][jumlah]"]').val(jumlah)
+                    $('[name="pengirimans['+index+'][satuan]"]').val(satuan)
+                    $('[name="pengirimans['+index+'][harga]"]').val(formatter(harga))
+                    $('[name="pengirimans['+index+'][total]"]').val(formatter(total))
+
+                    subtotal += total;
+                    $("#total").val(formatter(subtotal))
+
+                    if ((index + 1) == detail.length) {
+                        $("#pesanan_id").attr('disabled', false)
+                    }
                 })
             }
         })
--- resources/views/admin/sales/pengiriman/index.blade.php
+++ resources/views/admin/sales/pengiriman/index.blade.php
@@ -54,14 +54,15 @@
                                                 <i data-feather="edit"></i>
                                                 <span class="ml-1">Edit</span>
                                             </a>
-                                            <div class="dropdown-item">
-                                                <form action="{{ route('admin.sales.pengiriman.destroy', $pengiriman->id) }}" method="post">
-                                                    @csrf
-                                                    @method('delete')
-                                                    <button type="submit" class="button delete-confirm btn btn-flat-danger"><i data-feather="trash"></i>
-                                                        <span class="ml-1">Delete</span></button>
-                                                </form>
-                                            </div>
+                                            <a href="javascript:void('delete')" class="dropdown-item text-danger" 
+                                                onclick="deleteConfirm('form-delete', '{{ $pengiriman->id }}')">
+                                                <i data-feather="trash"></i>
+                                                <span class="ml-1">Delete</span>
+                                            </a>
+                                            <form id="form-delete{{ $pengiriman->id }}" action="{{ route('admin.sales.pengiriman.destroy', $pengiriman->id) }}" method="POST">
+                                                @csrf
+                                                @method('delete')
+                                            </form>
                                         </div>
                                     </div>
                                 </td>
