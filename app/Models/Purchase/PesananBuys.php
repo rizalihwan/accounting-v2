b37572c63755549099a2b8b4672605257a29304d
dhanifu <mramdhani010@gmail.com>
2021-07-17 16:37:02 +0700

    relasi ke penawaran

--- app/Models/Purchase/PesananBuys.php
+++ app/Models/Purchase/PesananBuys.php
@@ -8,9 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class PesananBuys extends Model
 {
     use HasFactory;
+
     protected $guarded = [];
+
     public function pemasok()
     {
         return $this->belongsTo(\App\Models\Kontak::class);
+    }
+
+    public function penawaran()
+    {
+        return $this->belongsTo(PenawaranBuys::class, 'penawaran_id');
     }
 }
