f73026818b688042ae8886db5ebad04ff0db3c17
rizalihwan <55536560+rizalihwan@users.noreply.github.com>
2021-07-21 16:10:33 +0700

    ngomeken card kas&bank, padahal waktu itu ges. Njir

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -698,23 +698,23 @@
 @endpush
 <section id="card-content-types">
     <div class="row">
-        <div class="col-md-3 col-xl-3">
+        <div class="col-md-4">
             <a href="{{ route('admin.bkk.index') }}">
                 <div class="card bg-warning border-0 text-white">
                     <div class="card-body">
                         <div class="title">
                             <div class="row">
-                                <div class="col">
-                                    <h1 class="text-light">Expense</h1>
+                                <div class="col-md-10">
+                                    <h1 class="text-light">Pengeluaran</h1>
                                 </div>
-                                <div class="col">
+                                <div class="col-md-2">
                                     <i class="fa fa-hand-holding-usd fa-lg fa-5x"></i>
                                 </div>
                             </div>
                         </div>
                         <div class="info">
                             <p class="card-text">
-                            <h1 class="card-text display-5 text-white font-weight-bold">Expense</h1>
+                            <h1 class="card-text display-5 text-white font-weight-bold">Pengeluaran</h1>
                             <i class="fa fa-hand-holding-usd"></i>
                             </p>
                             <hr class="mr-1">
@@ -729,28 +729,28 @@
                 </div>
             </a>
         </div>
-        <div class="col-md-3 col-xl-3">
+        <div class="col-md-4">
             <a href="{{ route('admin.bkm.index') }}">
                 <div class="card bg-success border-0 text-white">
                     <div class="card-body">
                         <div class="title">
                             <div class="row">
-                                <div class="col">
-                                    <h1 class="text-light">Income</h1>
+                                <div class="col-md-10">
+                                    <h1 class="text-light">Penerimaan</h1>
                                 </div>
-                                <div class="col">
+                                <div class="col-md-2">
                                     <i class="fa fa-hand-holding-usd fa-lg fa-5x"></i>
                                 </div>
                             </div>
                         </div>
                         <div class="info">
                             <p class="card-text">
-                            <h1 class="card-text display-5 text-white font-weight-bold">Income</h1>
+                            <h1 class="card-text display-5 text-white font-weight-bold">Penerimaan</h1>
                             <i class="fa fa-hand-holding-usd"></i>
                             </p>
                             <hr class="mr-1">
                             <p class="card-text">
-                                Mencatat pemasukan kas/bank
+                                Mencatat penerimaan kas/bank
                             </p>
                             <p class="card-text">
                                 <small class="text-light"></small>
