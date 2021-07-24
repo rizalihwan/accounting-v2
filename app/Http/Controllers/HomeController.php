3addd229f5d585126381422983ef98860bb9084c
dhanifu <mramdhani010@gmail.com>
2021-07-24 16:47:40 +0700

    Merge branch 'dev' of https://github.com/dhanifu/accounting into dhani

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,10 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\{Akun, Bkk, Jurnalumumdetail};
+use App\Models\Purchase\FakturBuy;
+use App\Models\Sale\FakturSale;
+
 class HomeController extends Controller
 {
     public function home()
     {
-        return view('home');
+        $sumSaldoAkhir = Akun::sum('saldo_akhir');
+        $sumJurnalUmum = Jurnalumumdetail::sum('debit');
+        $sumFakturPenjualan = FakturSale::sum('total');
+        $sumFakturPembelian = FakturBuy::sum('total');
+
+        $kerugianOrKeuntungan = $sumJurnalUmum + $sumFakturPenjualan - $sumFakturPembelian;
+        $penerimaanAtas = $sumJurnalUmum + $sumFakturPenjualan;
+        $result = $kerugianOrKeuntungan < 1 ? 'Kerugian' : 'Keuntungan';
+        $sumExpense = Bkk::where('status', 'BKK')->sum('value');
+        $sumIncome = Bkk::where('status', 'BKM')->sum('value');
+        return view('home', [
+            'saldoKeseluruhan' => $sumSaldoAkhir,
+            'kerugianOrKeuntungan' => $kerugianOrKeuntungan,
+            'result' => $result,
+            'penerimaanAtas' => $penerimaanAtas,
+            'pengeluaranAtas' => $sumFakturPembelian,
+            'penerimaanBawah' => $sumJurnalUmum + $sumExpense + $sumIncome,
+            'pengeluaranBawah' => $sumExpense + $sumIncome
+        ]);
     }
 }
--- resources/views/admin/bkk/edit.blade.php
+++ resources/views/admin/bkk/edit.blade.php
@@ -1,158 +1,169 @@
 @extends('_layouts.main')
 @section('title', 'Buku Kas Keluar')
-@push('breadcrumb')
-    <li class="breadcrumb-item">
-        <a href="{{ route('admin.cash-bank') }}">Cash & Bank</a>
-    </li>
-    <li class="breadcrumb-item active">
-        <a href="{{ route('admin.bkk.index') }}">Expanse</a>
-    </li>
-    <li class="breadcrumb-item active" aria-current="page">Edit</li>
-@endpush
+    @push('breadcrumb')
+        <li class="breadcrumb-item">
+            <a href="{{ route('admin.cash-bank') }}">Cash & Bank</a>
+        </li>
+        <li class="breadcrumb-item active">
+            <a href="{{ route('admin.bkk.index') }}">Expanse</a>
+        </li>
+        <li class="breadcrumb-item active" aria-current="page">Edit</li>
+    @endpush
 @section('content')
-        <div class="row">
-          <!-- end message area-->
-          <div class="col-md-12">
-              <form action="{{ route('admin.bkk.update', $datas->id) }}" method="POST" class="invoice-repeater">
-                  @csrf
-                  @method('put')
-                  <div class="card mb-2 ">
-                      <div class="card-header">
-                          <h4>Edit Expanse</h4>
-                      </div>
-                      <div class="card-body ml-4">
-                          <div class="row d-flex align-items-end">
-                              <div class="col-md-4 col-12 ">
-                                  <div class="form-group">
-                                      <label for="itemname">Tanggal</label>
-                                      <input type="date" class="form-control" name="tanggal" value="{{$datas->tanggal}}" required/>
-                                  </div>
-                              </div>
-                              <div class="col-md-3 col-12 ml-auto mr-4">
-                                  <div class="form-group">
-                                      <label for="id">No Jurnal</label>
-                                      <input type="text" class="form-control" placeholder="auto number" disabled />
-                                  </div>
-                              </div>
-                          </div>
-                          <div class="row d-flex align-items-end">
-                              <div class="col-md-4 col-12 ">
-                                  <div class="form-group">
-                                      <label for="kontak">Sudah Bayar Ke</label>
-                                      <select name="kontak" id="kontak" class="form-control" required>
-                                        <option value="{{$datas->kontak_id}}">{{$datas->kontaks->nama}}</option>
-                                          @foreach ($kontak as $item)
-                                          <option value="{{$item->id}}">{{$item->nama}}</option>
-                                          @endforeach
-                                      </select>
-                                  </div>
-                              </div>
-                              <div class="col-md-11 col-12 ">
-                                  <div class="form-group">
-                                      <label for="desk">Untuk Pembayaran</label>
-                                      <input type="text" class="form-control" name="desk" placeholder="Deskripsi keterangan" value="{{$datas->desk}}" required/>
-                                  </div>
-                              </div>
-                              <div class="col-md-4 col-12 ">
-                                  <div class="form-group">
-                                      <label for="kontak">Rek.Kas/Bank[K]</label>
-                                      <select name="rek" id="rek" class="form-control">
-                                        <option value="{{$datas->rekening_id}}">{{$datas->akuns->name}}-{{$datas->akuns->subklasifikasi->name}}</option>
-                                        @foreach ($rekening as $item)
-                                        @if (!empty($item->subklasifikasi->name))
-                                        <option value="{{$item->id}}">{{ $item->name }}-{{$item->subklasifikasi->name}}</option>
-                                        @endif
+    <div class="row">
+        <!-- end message area-->
+        <div class="col-md-12">
+            <form action="{{ route('admin.bkk.update', $datas->id) }}" method="POST" class="invoice-repeater">
+                @csrf
+                @method('put')
+                <div class="card mb-2 ">
+                    <div class="card-header">
+                        <h4>Edit Expanse</h4>
+                    </div>
+                    <div class="card-body ml-4">
+                        <div class="row d-flex align-items-end">
+                            <div class="col-md-4 col-12 ">
+                                <div class="form-group">
+                                    <label for="itemname">Tanggal</label>
+                                    <input type="date" class="form-control" name="tanggal" value="{{ $datas->tanggal }}"
+                                        required />
+                                </div>
+                            </div>
+                            <div class="col-md-3 col-12 ml-auto mr-4">
+                                <div class="form-group">
+                                    <label for="id">No Jurnal</label>
+                                    <input type="text" class="form-control" placeholder="auto number" disabled />
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row d-flex align-items-end">
+                            <div class="col-md-4 col-12 ">
+                                <div class="form-group">
+                                    <label for="kontak">Sudah Bayar Ke</label>
+                                    <select name="kontak" id="kontak" class="form-control" required>
+                                        <option value="{{ $datas->kontak_id }}">{{ $datas->kontaks->nama }}</option>
+                                        @foreach ($kontak as $item)
+                                            <option value="{{ $item->id }}">{{ $item->nama }}</option>
                                         @endforeach
-                                      </select>
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-                  <div class="card p-3">
-                      <div class="card-body mt-0">
-                          <hr>
-                          <div class="col-12 ">
-                              <div data-repeater-list="invoice">
-                                  @foreach ($datas->uraians as $data)
-                                  <div data-repeater-item>
-                                      <div class="row d-flex align-items-end">
-                                          <div class="col-md-4 col-12 ">
-                                              <div class="form-group">
-                                                  <label for="itemname">no rek</label>
-                                                  <select name="rekening" id="rekening" class="form-control">
-                                                    <option value="{{$data->rekening_id}}">{{$data->rekening->name}}-{{$data->rekening->subklasifikasi->name}}</option>
-                                                    @foreach ($rekenings as $item)
-                                                    <option value="{{$item->id}}">{{ $item->name }}-{{$item->subklasifikasi->name}}</option>
-                                                    @endforeach
-                                                  </select>
-                                              </div>
-                                          </div>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="col-md-11 col-12 ">
+                                <div class="form-group">
+                                    <label for="desk">Untuk Pembayaran</label>
+                                    <input type="text" class="form-control" name="desk" placeholder="Deskripsi keterangan"
+                                        value="{{ $datas->desk }}" required />
+                                </div>
+                            </div>
+                            <div class="col-md-4 col-12 ">
+                                <div class="form-group">
+                                    <label for="kontak">Rek.Kas/Bank[K]</label>
+                                    <select name="rek" id="rek" class="form-control">
+                                        <option value="{{ $datas->rekening_id }}">
+                                            {{ $datas->akuns->name }}-{{ $datas->akuns->subklasifikasi->name }}
+                                        </option>
+                                        @foreach ($rekening as $item)
+                                            @if (!empty($item->subklasifikasi->name))
+                                                <option value="{{ $item->id }}">
+                                                    {{ $item->name }}-{{ $item->subklasifikasi->name }}</option>
+                                            @endif
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card p-3">
+                    <div class="card-body mt-0">
+                        <hr>
+                        <div class="col-12 ">
+                            <div data-repeater-list="invoice">
+                                @foreach ($datas->uraians as $data)
+                                    <div data-repeater-item>
+                                        <div class="row d-flex align-items-end">
+                                            <div class="col-md-4 col-12 ">
+                                                <div class="form-group">
+                                                    <label for="itemname">no rek</label>
+                                                    <select name="rekening" id="rekening" class="form-control">
+                                                        <option value="{{ $data->rekening_id }}">
+                                                            {{ $data->rekening->name }}-{{ $data->rekening->subklasifikasi->name }}
+                                                        </option>
+                                                        @foreach ($rekenings as $item)
+                                                            <option value="{{ $item->id }}">
+                                                                {{ $item->name }}-{{ $item->subklasifikasi->name }}
+                                                            </option>
+                                                        @endforeach
+                                                    </select>
+                                                </div>
+                                            </div>
 
-                                          <div class="col-md-2 col-12" id="app">
-                                            <div class="form-group">
-                                                <label for="jumlah">Jumlah uang</label>
-                                                <input type="number" class="form-control jumlah" oninput="HowAboutIt()" placeholder="1" name="jumlah" value="{{$data->jml_uang}}" required/>
+                                            <div class="col-md-2 col-12" id="app">
+                                                <div class="form-group">
+                                                    <label for="jumlah">Jumlah uang</label>
+                                                    <input type="number" class="form-control jumlah" oninput="HowAboutIt()"
+                                                        placeholder="1" name="jumlah" value="{{ $data->jml_uang }}"
+                                                        required />
+                                                </div>
+                                            </div>
+
+                                            <div class="col-md-2 col-12">
+                                                <div class="form-group">
+                                                    <label for="catatan">catatan</label>
+                                                    <input type="text" class="form-control" id="catatan" name="catatan"
+                                                        value="{{ $data->catatan }}" />
+                                                </div>
+                                            </div>
+
+                                            <div class="col-md-2 col-12">
+                                                <div class="form-group">
+                                                    <label for="matauang">uang</label>
+                                                    <select name="matauang" id="matauang" class="form-control">
+                                                        <option value="RP">{{ $data->uang }}</option>
+                                                        <option value="RP">RP</option>
+                                                        <option value="USD">USD</option>
+                                                    </select>
+                                                </div>
+                                            </div>
+
+                                            <div class="col-md-2 col-12">
+                                                <div class="form-group">
+                                                    <button class="btn btn-outline-danger " onclick="calculate(event)"
+                                                        data-repeater-delete type="button">
+                                                        <i class="fa fa-times"></i>
+                                                        <span>Delete</span>
+                                                    </button>
+                                                </div>
                                             </div>
                                         </div>
-
-                                          <div class="col-md-2 col-12">
-                                              <div class="form-group">
-                                                  <label for="catatan">catatan</label>
-                                                  <input type="text" class="form-control" id="catatan" name="catatan" value="{{$data->catatan}}"/>
-                                              </div>
-                                          </div>
-
-                                          <div class="col-md-2 col-12">
-                                              <div class="form-group">
-                                                  <label for="matauang">uang</label>
-                                                  <select name="matauang" id="matauang" class="form-control">
-                                                    <option value="RP">{{$data->uang}}</option>
-                                                    <option value="RP">RP</option>
-                                                    <option value="USD">USD</option>
-                                                  </select>
-                                              </div>
-                                          </div>
-
-                                          <div class="col-md-2 col-12">
-                                              <div class="form-group">
-                                                  <button class="btn btn-outline-danger " onclick="calculate(event)" data-repeater-delete type="button">
-                                                      <i class="fa fa-times"></i>
-                                                      <span>Delete</span>
-                                                  </button>
-                                              </div>
-                                          </div>
-                                      </div>
-                                      <hr />
-                                  </div>
-                                  @endforeach
-                              </div>
-                              <div class="row ">
-                                  <div class="col-8">
-                                      <button class="btn btn-outline-primary" id="button" type="button" data-repeater-create>
-                                          <i class="ik ik-plus"></i>
-                                          <span>Add New</span>
-                                      </button>
-                                      <button type="submit ml-auto" class="btn btn-outline-primary">
-                                          Update</button>
-                                  </div>
-                                  <div class="d-flex">
-                                      <input type="text" class="form-control" id="total">
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-              </form>
-          </div>
-      </div>
+                                        <hr />
+                                    </div>
+                                @endforeach
+                            </div>
+                            <div class="row ">
+                                <div class="col-8">
+                                    <button class="btn btn-outline-primary" id="button" type="button" data-repeater-create>
+                                        <i class="ik ik-plus"></i>
+                                        <span>Add New</span>
+                                    </button>
+                                    <button type="submit ml-auto" class="btn btn-outline-primary">
+                                        Update</button>
+                                </div>
+                                <div class="d-flex">
+                                    <input type="text" class="form-control" id="total">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
     <script>
-      function HowAboutIt()
-        {
+        function HowAboutIt() {
             let total = 0;
             let coll = document.querySelectorAll('.jumlah')
-            for(let i = 0;i<coll.length;i++)
-            {
+            for (let i = 0; i < coll.length; i++) {
                 let ele = coll[i]
                 total += parseInt(ele.value)
             }
