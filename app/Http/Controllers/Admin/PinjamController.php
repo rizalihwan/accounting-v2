da2edb257c13bf37e9911b7cf75cb51891da4543
AlghanyJagad <alghanyjagad@gmail.com>
2021-08-11 14:25:52 +0700

    add simpanpinjam controller & view

---
+++ app/Http/Controllers/Admin/PinjamController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Pinjam;
+use Illuminate\Http\Request;
+
+class PinjamController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('admin.pinjam.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.pinjam.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Pinjam  $pinjam
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Pinjam $pinjam)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Pinjam  $pinjam
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Pinjam $pinjam)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Pinjam  $pinjam
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Pinjam $pinjam)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Pinjam  $pinjam
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Pinjam $pinjam)
+    {
+        //
+    }
+}
