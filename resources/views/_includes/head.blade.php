c1fe2bf6192cddd5b839e7562ba1810da2880d42
dhanifu <mramdhani010@gmail.com>
2021-08-13 03:30:04 +0700

    ubah nama app

--- resources/views/_includes/head.blade.php
+++ resources/views/_includes/head.blade.php
@@ -5,7 +5,7 @@
 <meta name="keywords" content="Aplikasi Akunting TWP AD, TWP AD, Akunting TWP AD">
 <meta name="author" content="Akunting">
 <meta name="csrf-token" content="{{ csrf_token() }}">
-<title>@yield('title', '') | Akunting</title>
+<title>@yield('title', '') | {{ config('app.name') }}</title>
 <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;1,400;1,500;1,600" rel="stylesheet">
 
 <!-- BEGIN: Vendor CSS-->
